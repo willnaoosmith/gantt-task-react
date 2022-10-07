16126cd51b62150d1f2e99b467fe3420ab2911b6
willnaoosmith <brochensquewill@protonmail.com>
2022-10-07 15:15:24 -0300

    testing

--- src/components/other/tooltip.tsx
+++ src/components/other/tooltip.tsx
@@ -125,16 +125,16 @@ export const StandardTooltipContent: React.FC<{
     <div className={styles.tooltipDefaultContainer} style={style}>
       <b style={{ fontSize: fontSize + 6 }}>{`${
         task.name
-      }: ${task.start.getDate()}-${
+      }: ${task.start.getDate()}/${
         task.start.getMonth() + 1
-      }-${task.start.getFullYear()} - ${task.end.getDate()}-${
+      }/${task.start.getFullYear()} - ${task.end.getDate()}/${
         task.end.getMonth() + 1
-      }-${task.end.getFullYear()}`}</b>
+      }/${task.end.getFullYear()}`}</b>
       {task.end.getTime() - task.start.getTime() !== 0 && (
-        <p className={styles.tooltipDefaultContainerParagraph}>{{`Duração: ${~~(
+        <p className={styles.tooltipDefaultContainerParagraph}>{`Duração: ${~~(
           (task.end.getTime() - task.start.getTime()) /
           (1000 * 60 * 60 * 24)
-        )} day(s)`}.replace("-", "/")}</p>
+        )} day(s)`}</p>
       )}
 
       <p className={styles.tooltipDefaultContainerParagraph}>
