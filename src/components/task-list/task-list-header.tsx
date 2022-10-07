64ca134ef9ee7a940793c78b7da87eb6b54951b4
willnaoosmith <brochensquewill@protonmail.com>
2022-10-07 09:40:12 -0300

    testing

--- src/components/task-list/task-list-header.tsx
+++ src/components/task-list/task-list-header.tsx
@@ -27,7 +27,7 @@ export const TaskListHeaderDefault: React.FC<{
             minWidth: rowWidth,
           }}
         >
-          &nbsp;Tarefa
+          &nbsp;Teste
         </div>
         <div
           className={styles.ganttTable_HeaderSeparator}
